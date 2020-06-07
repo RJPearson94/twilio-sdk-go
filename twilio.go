707968aaa60537cb32f1a353a2951ab5684813f1
RJPearson94 <RJPearson94@users.noreply.github.com>
2020-06-07 15:43:20 +0100

    feat: add taskrouter into the root twilio client

--- twilio.go
+++ twilio.go
@@ -2,17 +2,20 @@ package twilio
 
 import (
 	"github.com/RJPearson94/twilio-sdk-go/service/studio"
+	"github.com/RJPearson94/twilio-sdk-go/service/taskrouter"
 	"github.com/RJPearson94/twilio-sdk-go/session"
 	"github.com/RJPearson94/twilio-sdk-go/session/credentials"
 )
 
 type Twilio struct {
-	Studio *studio.Studio
+	Studio     *studio.Studio
+	TaskRouter *taskrouter.TaskRouter
 }
 
 func New(sess *session.Session) *Twilio {
 	c := &Twilio{}
 	c.Studio = studio.New(sess)
+	c.TaskRouter = taskrouter.New(sess)
 	return c
 }
 
