a1524dc4603b165e098be7c1055eaca1a4579759
RJPearson94 <robpearson@pearson.co.uk>
2020-06-09 21:41:14 +0100

    fix: correct property name

--- service/taskrouter/v1/workspace/worker/api_op_get.go
+++ service/taskrouter/v1/workspace/worker/api_op_get.go
@@ -10,18 +10,18 @@ import (
 )
 
 type GetWorkerOutput struct {
-	Sid              string      `json:"sid"`
-	AccountSid       string      `json:"account_sid"`
-	WorkspaceSid     string      `json:"workspace_sid"`
-	ActivitySid      string      `json:"activity_sid"`
-	FriendlyName     string      `json:"friendly_name"`
-	ActivityName     string      `json:"activity_name"`
-	Attributes       interface{} `json:"attributes"`
-	Available        bool        `json:"available"`
-	DateCreated      time.Time   `json:"date_created"`
-	DateUpdated      *time.Time  `json:"date_updated,omitempty"`
-	DateStatusChange *time.Time  `json:"date_status_changed,omitempty"`
-	URL              string      `json:"url"`
+	Sid               string      `json:"sid"`
+	AccountSid        string      `json:"account_sid"`
+	WorkspaceSid      string      `json:"workspace_sid"`
+	ActivitySid       string      `json:"activity_sid"`
+	FriendlyName      string      `json:"friendly_name"`
+	ActivityName      string      `json:"activity_name"`
+	Attributes        interface{} `json:"attributes"`
+	Available         bool        `json:"available"`
+	DateCreated       time.Time   `json:"date_created"`
+	DateUpdated       *time.Time  `json:"date_updated,omitempty"`
+	DateStatusChanged *time.Time  `json:"date_status_changed,omitempty"`
+	URL               string      `json:"url"`
 }
 
 func (c Client) Get() (*GetWorkerOutput, error) {
