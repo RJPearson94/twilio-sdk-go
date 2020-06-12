50c464fc99ec4f987f28a0a489337e5c44983cf7
RJPearson94 <robpearson@pearson.co.uk>
2020-06-12 01:30:41 +0100

    feat: add deployment resource

--- service/serverless/v1/service/environment/api_op_client.go
+++ service/serverless/v1/service/environment/api_op_client.go
@@ -3,24 +3,32 @@ package environment
 
 import (
 	"github.com/RJPearson94/twilio-sdk-go/client"
+	"github.com/RJPearson94/twilio-sdk-go/service/serverless/v1/service/environment/deployment"
+	"github.com/RJPearson94/twilio-sdk-go/service/serverless/v1/service/environment/deployments"
 	"github.com/RJPearson94/twilio-sdk-go/service/serverless/v1/service/environment/variable"
 	"github.com/RJPearson94/twilio-sdk-go/service/serverless/v1/service/environment/variables"
 )
 
 type Client struct {
-	client     *client.Client
-	serviceSid string
-	sid        string
-	Variables  *variables.Client
-	Variable   func(string) *variable.Client
+	client      *client.Client
+	serviceSid  string
+	sid         string
+	Variables   *variables.Client
+	Variable    func(string) *variable.Client
+	Deployments *deployments.Client
+	Deployment  func(string) *deployment.Client
 }
 
 func New(client *client.Client, serviceSid string, sid string) *Client {
 	return &Client{
-		client:     client,
-		serviceSid: serviceSid,
-		sid:        sid,
-		Variables:  variables.New(client, sid, serviceSid),
-		Variable:   func(variableSid string) *variable.Client { return variable.New(client, sid, serviceSid, variableSid) },
+		client:      client,
+		serviceSid:  serviceSid,
+		sid:         sid,
+		Variables:   variables.New(client, sid, serviceSid),
+		Variable:    func(variableSid string) *variable.Client { return variable.New(client, sid, serviceSid, variableSid) },
+		Deployments: deployments.New(client, sid, serviceSid),
+		Deployment: func(deploymentSid string) *deployment.Client {
+			return deployment.New(client, sid, serviceSid, deploymentSid)
+		},
 	}
 }
