4df0dcdf2e7819b0e1a0d4bdab05a7f98cadca3f
RJPearson94 <RJPearson94@users.noreply.github.com>
2020-06-12 01:15:29 +0100

    feat: add severless variable resources

--- service/serverless/v1/service/environment/api_op_client.go
+++ service/serverless/v1/service/environment/api_op_client.go
@@ -3,12 +3,16 @@ package environment
 
 import (
 	"github.com/RJPearson94/twilio-sdk-go/client"
+	"github.com/RJPearson94/twilio-sdk-go/service/serverless/v1/service/environment/variable"
+	"github.com/RJPearson94/twilio-sdk-go/service/serverless/v1/service/environment/variables"
 )
 
 type Client struct {
 	client     *client.Client
 	serviceSid string
 	sid        string
+	Variables  *variables.Client
+	Variable   func(string) *variable.Client
 }
 
 func New(client *client.Client, serviceSid string, sid string) *Client {
@@ -16,5 +20,7 @@ func New(client *client.Client, serviceSid string, sid string) *Client {
 		client:     client,
 		serviceSid: serviceSid,
 		sid:        sid,
+		Variables:  variables.New(client, sid, serviceSid),
+		Variable:   func(variableSid string) *variable.Client { return variable.New(client, sid, serviceSid, variableSid) },
 	}
 }
