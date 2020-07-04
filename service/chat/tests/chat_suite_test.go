6fdb2335ae8dd34c90312544a46490bbdb7c07e9
RJPearson94 <RJPearson94@users.noreply.github.com>
2020-07-04 21:08:38 +0100

    chore: correct typo

--- service/chat/tests/chat_suite_test.go
+++ service/chat/tests/chat_suite_test.go
@@ -7,7 +7,7 @@ import (
 	. "github.com/onsi/gomega"
 )
 
-func TestV1(t *testing.T) {
+func TestV2(t *testing.T) {
 	RegisterFailHandler(Fail)
-	RunSpecs(t, "Proxy Test Suite")
+	RunSpecs(t, "Chat Test Suite")
 }
