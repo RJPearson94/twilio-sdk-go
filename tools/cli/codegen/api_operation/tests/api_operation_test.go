2d6db2de1f57e8adbfe1fd71683312861ebf7bf9
RJPearson94 <RJPearson94@users.noreply.github.com>
2020-08-15 23:19:25 +0100

    chore: remove println in test

--- tools/cli/codegen/api_operation/tests/api_operation_test.go
+++ tools/cli/codegen/api_operation/tests/api_operation_test.go
@@ -2,7 +2,6 @@ package tests
 
 import (
 	"encoding/json"
-	"fmt"
 	"io/ioutil"
 
 	"github.com/Jeffail/gabs/v2"
@@ -44,7 +43,6 @@ var _ = Describe("API Operation CodeGen", func() {
 			})
 
 			It("Then the response should match the golden data", func() {
-				fmt.Println(string(*resp))
 				Expect(string(*resp)).To(Equal(string(goldenData)))
 			})
 		})
