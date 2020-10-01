340c235e0f5ebebe42bd368830d009d8aca3af63
RJPearson94 <RJPearson94@users.noreply.github.com>
2020-10-01 13:27:46 +0100

    chore: add conference examples

---
+++ examples/service/api/conference/update/conference_update_example.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/RJPearson94/twilio-sdk-go"
+	v2010 "github.com/RJPearson94/twilio-sdk-go/service/api/v2010"
+	"github.com/RJPearson94/twilio-sdk-go/service/api/v2010/account/conference"
+	"github.com/RJPearson94/twilio-sdk-go/session/credentials"
+	"github.com/RJPearson94/twilio-sdk-go/utils"
+)
+
+var apiSession *v2010.V2010
+
+func init() {
+	creds, err := credentials.New(credentials.Account{
+		Sid:       os.Getenv("TWILIO_ACCOUNT_SID"),
+		AuthToken: os.Getenv("TWILIO_AUTH_TOKEN"),
+	})
+	if err != nil {
+		log.Panicf("%s", err.Error())
+	}
+
+	apiSession = twilio.NewWithCredentials(creds).API.V2010
+}
+
+func main() {
+	resp, err := apiSession.
+		Account("ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX").
+		Conference("CFXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX").
+		Update(&conference.UpdateConferenceInput{
+			Status: utils.String("completed"),
+		})
+
+	if err != nil {
+		log.Panicf("%s", err.Error())
+	}
+
+	log.Printf("SID: %s", resp.Sid)
+}
